128847f9dc6006e9508d6fda0422ce676692215b
CarlosEDP <me@carlosedp.com>
2015-04-01 15:36:03 -0300

    Updated headers. Removed unused file

--- apps/dccaserver/src/diameter_srv.erl
+++ apps/dccaserver/src/diameter_srv.erl
@@ -5,6 +5,7 @@
 %% @author Carlos Eduardo de Paula <carlosedp@gmail.com>
 %% @copyright 2015 Carlos Eduardo de Paula
 %% @doc gen_server
+%%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
 %% except in compliance with the License. You may obtain

50d15b9a1156c452c76636e1afd2b18e6812c269
CarlosEDP <me@carlosedp.com>
2015-04-01 15:25:29 -0300

    Updated headers

--- apps/dccaserver/src/dccaserver_sup.erl
+++ apps/dccaserver/src/dccaserver_sup.erl
@@ -1,13 +1,30 @@
-%-*- mode: erlang -*-
-%%%'   HEADER
+%% -------------------------------------------------------------------
+%%
+%% dccaserver: Erlang OTP Diameter Credit Control Application
+%%
 %% @author Carlos Eduardo de Paula <carlosedp@gmail.com>
 %% @copyright 2015 Carlos Eduardo de Paula
-%% @doc gen_server callback module implementation:
+%% @doc OTP supervisor
 %%
-%% @end
--module(dccaserver_sup).
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License. You may obtain
+%% a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied. See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 
+-module(dccaserver_sup).
 -behaviour(supervisor).
+
 -export([start_link/0]).
 -export([init/1]).
 
--- apps/dccaserver/src/diameter_srv.erl
+++ apps/dccaserver/src/diameter_srv.erl
@@ -1,11 +1,27 @@
+%% -------------------------------------------------------------------
+%%
+%% dccaserver: Erlang OTP Diameter Credit Control Application
+%%
 %% @author Carlos Eduardo de Paula <carlosedp@gmail.com>
 %% @copyright 2015 Carlos Eduardo de Paula
-%% @doc gen_server callback module implementation:
+%% @doc gen_server
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License. You may obtain
+%% a copy of the License at
 %%
-%% @end
--module(diameter_srv).
--author('Carlos Eduardo de Paula <carlosedp@gmail.com>').
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied. See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 
+-module(diameter_srv).
 -behaviour(gen_server).
 
 -include_lib("diameter/include/diameter.hrl").

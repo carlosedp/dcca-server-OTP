8b39452d04e4a6fae875d049779a1c463ac3ef07
CarlosEDP <me@carlosedp.com>
2015-05-04 17:58:55 -0300

    Read configuration from environment variables

--- apps/dccaserver/include/diameter_settings.hrl
+++ apps/dccaserver/include/diameter_settings.hrl
@@ -23,9 +23,10 @@
 %% -------------------------------------------------------------------
 
 -define(DCCA_APPLICATION_ID, 4).
--define(DIAMETER_IP, "127.0.0.1").
--define(DIAMETER_PORT, 3868).
--define(DIAMETER_PROTO, tcp).
+-define(DIAMETER_IP, element(2,inet_parse:address(application:get_env(dccaserver, diameter_ip, "127.0.0.1")))).
+-define(DIAMETER_PORT, application:get_env(dccaserver, diameter_port, 3868)).
+-define(DIAMETER_PROTO, application:get_env(dccaserver, diameter_proto, tcp)).
+
 -define(VENDOR_ID, 0).
 -define(ORIGIN_HOST, "example.com").
 -define(ORIGIN_REALM, "realm.example.com").
